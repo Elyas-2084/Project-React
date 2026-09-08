231377e4a710ba2b75d6af3a56804ef66030d01d
Elias-2084 <elyas.pur.2020@gmail.com>
2026-09-08 19:18:27 +0330

    update fetchData

--- frontend/src/Utils/FetchData.js
+++ frontend/src/Utils/FetchData.js
@@ -1,13 +1,21 @@
 const fetchData = async (url, options = {}) => {
     try {
-        const res = await fetch(process.env.REACT_APP_BASE_API + url, options)
+        const fullUrl = process.env.REACT_APP_BASE_API + url
+
+        console.log('API REQUEST:', fullUrl)
+
+        const res = await fetch(fullUrl, options)
+
+        console.log('STATUS:', res.status)
+
         const data = await res.json()
+
+        console.log('API RESPONSE:', data)
+
         return data
-    }
-    catch (error) {
-        console.log(error)
+    } catch (error) {
+        console.log('FETCH ERROR:', error)
     }
 }
 
 export default fetchData
-
